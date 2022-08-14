5c987ef31b48af4127c40b24a849e3495fa92f39
SuphawitCE <b4nknaja@hotmail.com>
2022-08-14 23:42:30 +0700

    refactor(JS): Refactor all files

--- 7_subclasses.js
+++ 7_subclasses.js
@@ -1,31 +1,26 @@
-
 class Book {
-    constructor(title, author, year) {
-        this.title = title;
-        this.author = author;
-        this.year = year;
-    }
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
 
-    // Add method to a class
-    getSummary() {
-        return `${this.title} was written by ${this.author}
+  // Add method to a class
+  getSummary() {
+    return `${this.title} was written by ${this.author}
          in ${this.year}`;
-    }
-
+  }
 }
 
 //  Magazine Subclass
 class Magazine extends Book {
-    constructor(title, author, year, month) {
-        super(title, author, year);
-        this.month = month;
-    }
-
-
+  constructor(title, author, year, month) {
+    super(title, author, year);
+    this.month = month;
+  }
 }
 
 //  Instantiate Magazine
 const mag1 = new Magazine('Mag 1', 'John D', '2011', 'Jan');
 
 console.log(mag1.getSummary());
-
--- problems/js103.js
+++ problems/js103.js
@@ -1,13 +1,7 @@
-
-let point = { x : 10, y : 20};
+let point = { x: 10, y: 20 };
 
 // console.log(__proto__);
 
-let p3D = { z : 30, __proto__ : point };
+let p3D = { z: 30, __proto__: point };
 
 console.log(p3D, p3D.x, p3D.y, p3D.z, p3D.__proto__);
-
-
-
-
-
